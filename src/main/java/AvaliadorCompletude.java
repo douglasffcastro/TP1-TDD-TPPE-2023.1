95c62b1725a78695004229cab901aded064d7d88
Douglas Castro <farias.douglascastro@gmail.com>
2023-05-24 00:01:38 -0300

    Implementação do método adicionarComposto

--- src/main/java/AvaliadorCompletude.java
+++ src/main/java/AvaliadorCompletude.java
@@ -1,29 +1,42 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class AvaliadorCompletude {
-    Map<String, String> campoAtomico = new HashMap<String, String>();
+    Map<String, String> camposAtomicos = new HashMap<String, String>();
+    Map<String, List<Object>> camposCompostos = new HashMap<String, List<Object>>();
+
 
     public void adicionarAtomico(String nomecampo, String valor) {
-        campoAtomico.put(nomecampo, valor);
+        camposAtomicos.put(nomecampo, valor);
     }
 
-    public Map<String, String> getCampoAtomico() {
-        return campoAtomico;
-    }
-
-    public float calcularCompletudeAtomicos() {
-        if (this.campoAtomico.get("title") != null && this.campoAtomico.get("publicationDate") != null && this.campoAtomico.get("language") != null){
-            return 1F;
-        } else if (this.campoAtomico.get("title") != null && this.campoAtomico.get("publicationDate") != null
-                || this.campoAtomico.get("publicationDate") != null && this.campoAtomico.get("language") != null
-                || this.campoAtomico.get("title") != null && this.campoAtomico.get("language") != null){
-            return 0.66F;
-        } else if (this.campoAtomico.get("title") != null || this.campoAtomico.get("publicationDate") != null || this.campoAtomico.get("language") != null){
-            return 0.33F;
+    public void adicionarComposto(String campo, Object valor) {
+        if (camposCompostos.containsKey(campo)) {
+            List<Object> valoresExistente = camposCompostos.get(campo);
+            valoresExistente.add(valor);
         } else {
-            return 0F;
+            List<Object> novosValores = new ArrayList<>();
+            novosValores.add(valor);
+            camposCompostos.put(campo, novosValores);
+        }
+    }
+
+    public Map<String, String> getCamposAtomicos() {
+        return camposAtomicos;
+    }
+
+    public Map<String, List<Object>> getCamposCompostos() {
+        return camposCompostos;
+    }
+
+    public int calcularCompletudeAtomicos() {
+        if (this.camposAtomicos.get("title") != null && this.camposAtomicos.get("publicationDate") != null && this.camposAtomicos.get("language") != null){
+            return 3;
+        } else if (this.camposAtomicos.get("title") != null && this.camposAtomicos.get("publicationDate") != null || this.camposAtomicos.get("publicationDate") != null && this.camposAtomicos.get("language") != null || this.camposAtomicos.get("title") != null && this.camposAtomicos.get("language") != null){
+            return 2;
+        } else if (this.camposAtomicos.get("title") != null || this.camposAtomicos.get("publicationDate") != null || this.camposAtomicos.get("language") != null){
+            return 1;
+        } else {
+            return 0;
         }
     }
 }
---
+++ src/test/java/CompletudeCompostoTests.java
@@ -0,0 +1,107 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class CompletudeCompostoTests {
+
+    AvaliadorCompletude avaliadorCompletude;
+    Object[][] camposCompostos;
+    List<Object> valorEsperado;
+
+    @Before
+    public void setup() {
+        avaliadorCompletude = new AvaliadorCompletude();
+    }
+
+    public CompletudeCompostoTests(Object[][] camposCompostos, List<Object> valorEsperado) {
+        this.camposCompostos = camposCompostos;
+        this.valorEsperado = valorEsperado;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getParameters() {
+        String campo = "authors";
+        Object[][] respostas = new Object[][] {
+                {new Object[][] {{"authors", new Object[] {}}}, List.of((Object) new Object[]{})},
+                {
+                    new Object[][] {
+                            {"authors", new Object[] {"name", "Sheila Albert dos Reis", "ordemAutoria", "1", "citationName", new String[]{"REIS, S. A."}}},
+                    },
+                    List.of((Object) new Object[]{"name", "Sheila Albert dos Reis", "ordemAutoria", "1", "citationName", new String[]{"REIS, S. A."}})
+                },
+                {
+                    new Object[][] {
+                        {"authors", new Object[] {"name", "Sheila Albert dos Reis", "ordemAutoria", "1", "citationName", new String[]{"REIS, S. A."}}},
+                        {"authors", new Object[] {"name", "E. D. C. Cavalcanti", "ordemAutoria", "1", "citationName", new String[]{"CAVALCANTI, E. D. C."}}}
+                    },
+                    Arrays.asList(
+                        (Object) new Object[]{"name", "Sheila Albert dos Reis", "ordemAutoria", "1", "citationName", new String[]{"REIS, S. A."}},
+                        (Object) new Object[]{"name", "E. D. C. Cavalcanti", "ordemAutoria", "1", "citationName", new String[]{"CAVALCANTI, E. D. C."}}
+                    )
+                },
+                {
+                    new Object[][] {
+                        {"authors", new Object[] {"name", "Sheila Albert dos Reis", "ordemAutoria", "1", "citationName", new String[]{"REIS, S. A."}}},
+                        {"authors", new Object[] {"name", "E. D. C. Cavalcanti", "ordemAutoria", "1", "citationName", new String[]{"CAVALCANTI, E. D. C."}}},
+                        {"authors", new Object[] {
+                                "name", "Dulce Helena Gonçalves Orofino",
+                                "ordemAutoria", "10",
+                                "citationName", new String[]{"OROFINO, D. H. G.",
+                                                            "OROFINO, DH",
+                                                            "OROFINO, DULCE HELENA GONÇALVES",
+                                                            "OROFINO, DULCE H. G.",
+                                                            "OROFINO, DULCE"
+                                                             },
+                                "identifier.lattes", "0009277151089005",
+                                "identifier.orcid", "0000-0002-0953-1831",
+                                "nationality", "Brasil",
+                                "birthCity", "rio de janeiro",
+                                "birthState", "RJ",
+                                "birthCountry", "Brasil"
+                                }
+                        }
+                    },
+                    Arrays.asList(
+                        (Object) new Object[]{"name", "Sheila Albert dos Reis", "ordemAutoria", "1", "citationName", new String[]{"REIS, S. A."}},
+                        (Object) new Object[]{"name", "E. D. C. Cavalcanti", "ordemAutoria", "1", "citationName", new String[]{"CAVALCANTI, E. D. C."}},
+                        (Object) new Object[]{
+                                "name", "Dulce Helena Gonçalves Orofino",
+                                "ordemAutoria", "10",
+                                "citationName", new String[]{"OROFINO, D. H. G.",
+                                "OROFINO, DH",
+                                "OROFINO, DULCE HELENA GONÇALVES",
+                                "OROFINO, DULCE H. G.",
+                                "OROFINO, DULCE"
+                        },
+                                "identifier.lattes", "0009277151089005",
+                                "identifier.orcid", "0000-0002-0953-1831",
+                                "nationality", "Brasil",
+                                "birthCity", "rio de janeiro",
+                                "birthState", "RJ",
+                                "birthCountry", "Brasil"
+                        }
+                    )
+                }
+        };
+
+        return Arrays.asList(respostas);
+    }
+
+    @Test
+    public void testAdicionaCamposAtomicos () {
+        for (Object[] campo: camposCompostos) {
+            avaliadorCompletude.adicionarComposto((String) campo[0], campo[1]);
+        }
+
+        List<Object> campoCompostoAtual = avaliadorCompletude.getCamposCompostos().get("authors");
+
+        Assertions.assertArrayEquals(valorEsperado.toArray(), campoCompostoAtual.toArray());
+    }
+}
